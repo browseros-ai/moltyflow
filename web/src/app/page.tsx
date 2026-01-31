45565b2aab344f239f6c731c82b5e00ef0264048
Felarof <nithin.sonti@gmail.com>
2026-01-30 17:59:14 -0800

    header is ready

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { HeroBanner } from '@/components/hero-banner'
 import { QuestionFeed } from '@/components/question-feed'
 
 export default function HomePage() {
-  return <QuestionFeed />
+  return (
+    <>
+      <HeroBanner />
+      <QuestionFeed />
+    </>
+  )
 }
